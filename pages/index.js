76fbed445931937dea377455fed375714bf72344
michael.w1 <winer.mike@gmail.com>
2022-02-11 16:41:41 -0500

    secret

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-        Welcome to secret {process.env.hello}
+        Welcome to secret {process.env.test_secret}
         </h1>
       </main>
 
